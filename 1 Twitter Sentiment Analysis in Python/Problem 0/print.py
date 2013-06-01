ea89c4718851801a65c2d3b602733c156b530da8
Yang Zhang <zhangyangcolostate@gmail.com>
2013-06-01 17:48:09 -0500

    updated print.py

--- 1 Twitter Sentiment Analysis in Python/Problem 0/print.py
+++ 1 Twitter Sentiment Analysis in Python/Problem 0/print.py
@@ -4,19 +4,19 @@ import json
 response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
 ##print json.load(response)
 
-numPages = 10;
+numPages = 2;
 
 for i in range(1,numPages+1):
     print 'Page: ' + str(i)
     url = 'http://search.twitter.com/search.json?q=microsoft&page=' + str(i)
     response = urllib.urlopen(url)
     data = json.load(response)
-    print "type(data): " + str(type(data))
-    print 'data.keys(): ' + str(data.keys())
+##    print "type(data): " + str(type(data))
+##    print 'data.keys(): ' + str(data.keys())
     tweets = data['results']
-    print 'tweets: ' + str(tweets)
-    print 'type(tweets): ' + str(type(tweets))
+##    print 'tweets: ' + str(tweets)
+##    print 'type(tweets): ' + str(type(tweets))
     for t in tweets:
-        print type(t)
+##        print type(t)
         print t['text']
     
